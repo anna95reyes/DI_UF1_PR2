ca492537a97ab912b1eddcb51153df4e773c191a
Anna <areyes@milaifontanals.org>
2021-11-09 15:10:44 +0100

    Agregar archivos de proyecto.

---
+++ Carcasone/Model/Fitxa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcasone.Model
+{
+    public class Fitxa
+    {
+
+        public static Boolean isStartingTile;
+
+        private int codi;
+        private string title;
+        private string imagePath;
+        private int repeticions;
+        private string notes;
+        private SideType[] sides;
+        private Boolean isMonastery;
+
+    }
+}
---
+++ Carcasone/Model/SideType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcasone.Model
+{
+    public enum SideType
+    {
+        CASTLE,
+        PATH,
+        FIELD
+    }
+}
