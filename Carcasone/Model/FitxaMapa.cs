45870deeecd380f9a811f0080b75648be063bf8d
Anna <areyes@milaifontanals.org>
2021-11-11 15:10:49 +0100

    211109_PreparacioClases

---
+++ Carcasone/Model/FitxaMapa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcasone.Model
+{
+    public class FitxaMapa
+    {
+        private Fitxa fitxa;
+        private Point posMapa;
+
+
+        /// <summary>
+        /// 4 valors possibles:
+        /// 0: sense rotar
+        /// 1: 90 graus
+        /// 2: 180 graus
+        /// 3: 270 graus
+        /// </summary>
+        private int rotacio = 0;
+        private SideType[] sides;
+
+        /// <summary>
+        /// Si la fitxa conté un nuno llabors
+        /// elNino != null
+        /// </summary>
+
+        private Nino elNino;
+
+    }
+}
---
+++ Carcasone/Model/Nino.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carcasone.Model
+{
+    public class Nino
+    {
+        /// <summary>
+        /// 0,1,2,3
+        /// </summary>
+        private PosType pos;
+        private int player;
+    }
+}
